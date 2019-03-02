f4167d2abb0dc9dc2cd27d85a0993273a01e791f
ZReKoJ <zhong.9745@gmail.com>
2019-03-02 01:31:29 +0100

    added pagerview on main activity for slide pages

--- src/main/java/com/ucm/tfg/MainActivity.java
+++ src/main/java/com/ucm/tfg/MainActivity.java
@@ -1,19 +1,42 @@
 package com.ucm.tfg;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        LeftFragment.OnFragmentInteractionListener,
+        CenterFragment.OnFragmentInteractionListener,
+        RightFragment.OnFragmentInteractionListener
+{
+
+    private ViewPager viewPager;
+    private PagerAdapter pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.container);
+        pagerAdapter = new SwipeAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setCurrentItem(1);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(view -> {
+            startActivity(new Intent(this, UnityPlayerActivity.class));
+        });
     }
-    public void lanzaAr(View view){
-        Intent ar = new Intent(this, UnityPlayerActivity.class);
-        startActivity(ar);
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
---
+++ src/main/java/com/ucm/tfg/SwipeAdapter.java
@@ -0,0 +1,28 @@
+package com.ucm.tfg;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SwipeAdapter extends FragmentPagerAdapter {
+
+    public  SwipeAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch(position) {
+            case 0: return new LeftFragment();
+            case 1: return new CenterFragment();
+            case 2: return new RightFragment();
+            default: return null;
+        }
+    }
+
+}
