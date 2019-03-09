b722cc87c7667f316c63acbd46a3ce35c36b45c0
ZReKoJ <zhong.9745@gmail.com>
2019-03-09 15:24:07 +0100

    removed retrofit dependencies

--- src/main/java/com/ucm/tfg/activities/SavedFilmActivity.java
+++ src/main/java/com/ucm/tfg/activities/SavedFilmActivity.java
@@ -9,20 +9,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
-import com.google.gson.FieldNamingPolicy;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.ucm.tfg.R;
-import com.ucm.tfg.entities.Film;
-import com.ucm.tfg.restClient.RestClient;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 public class SavedFilmActivity extends AppCompatActivity {
 
     private static String LOGTAG = "SavedFilmActivity";
@@ -48,42 +38,6 @@ public class SavedFilmActivity extends AppCompatActivity {
         } catch (JSONException e) {
             Log.e(LOGTAG, "Error at parsing");
         }
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .create();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://tfg-spring.herokuapp.com")
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-        RestClient restClient = retrofit.create(RestClient.class);
-        Call<Film> call = restClient.getData(this.uuid);
-
-        call.enqueue(new Callback<Film>() {
-            @Override
-            public void onResponse(Call<Film> call, Response<Film> response) {
-                switch (response.code()) {
-                    case 200:
-
-                        Film data = response.body();
-                        Log.i(LOGTAG, response.body().toString());
-                        saved_film_info.setText(data.getName());
-                        break;
-                    case 401:
-
-                        break;
-                    default:
-
-                        break;
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Film> call, Throwable t) {
-                Log.e("error", t.toString());
-            }
-        });
     }
 
 }
