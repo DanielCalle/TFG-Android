d6b4951f0dfcde79b5cf6a048ac68ce3d4f6293a
carlosgocereceda <carlosgcereceda@hotmail.es>
2019-03-19 16:55:15 +0100

    friends for unity

--- src/main/java/com/ucm/tfg/service/FriendshipService.java
+++ src/main/java/com/ucm/tfg/service/FriendshipService.java
@@ -1,14 +1,17 @@
 package com.ucm.tfg.service;
 
+import android.app.Activity;
+
 public class FriendshipService {
 
     private static String url = "";
-    private static String develop_url = "http://filmar-develop.herokuapp.com/friendship/";
+    private static String develop_url = "http://filmar-develop.herokuapp.com/user/friendships/";
 
     public FriendshipService() {}
 
-    public static <T> void getFriendsById(String uuid, Service.ClientResponse<T> callback, Class<T> c) {
+    public static <T> void getFriendsById(Activity activity, String uuid, Service.ClientResponse<T> callback, Class<T> c) {
         Service.getInstance()
+                .setContext(activity)
                 .addParam("id", uuid)
                 .GET(FriendshipService.develop_url + "{id}", callback, c);
     }
