7a39dc25a69036fb1ca263cfd4b0a614c29808a5
Dacuna97 <diegoacunaberger@gmail.com>
2019-03-22 16:19:17 +0100

    cardView styled for plans

--- src/main/java/com/ucm/tfg/adapters/PlanAdapter.java
+++ src/main/java/com/ucm/tfg/adapters/PlanAdapter.java
@@ -1,6 +1,8 @@
 package com.ucm.tfg.adapters;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -12,9 +14,12 @@ import android.widget.TextView;
 import com.ucm.tfg.R;
 import com.ucm.tfg.Utils;
 import com.ucm.tfg.entities.Plan;
+import com.ucm.tfg.entities.User;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import jp.wasabeef.blurry.Blurry;
 
 public class PlanAdapter extends RecyclerView.Adapter<PlanAdapter.RecyclerViewHolder> {
 
@@ -36,14 +41,22 @@ public class PlanAdapter extends RecyclerView.Adapter<PlanAdapter.RecyclerViewHo
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerViewHolder recyclerViewHolder, int i) {
-        Plan plan = plans.get(i);
+    public void onBindViewHolder(@NonNull RecyclerViewHolder recyclerViewHolder, int index) {
+        Plan plan = plans.get(index);
+
 
         Utils.convertImage(plan.getFilm().getImage(), recyclerViewHolder.image);
 
         recyclerViewHolder.title.setText(plan.getFilm().getName());
         recyclerViewHolder.from.setText(plan.getCreator().getName());
-        recyclerViewHolder.to.setText(plan.getFilm().getdirector());
+
+        List<User> joinedUsers = plan.getJoinedUsers();
+
+        String users = "";
+        for(int i = 0; i < joinedUsers.size(); i++){
+            users += joinedUsers.get(i).getName() + ", ";
+        }
+        recyclerViewHolder.to.setText(users);
     }
 
     @Override
