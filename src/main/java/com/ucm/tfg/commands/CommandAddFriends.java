a1ddfe45a55d5bdf94af9dc04f23f1e946476997
carlosgocereceda <carlosgcereceda@hotmail.es>
2019-03-25 19:59:41 +0100

    addfriend assets from unity

--- src/main/java/com/ucm/tfg/commands/CommandAddFriends.java
+++ src/main/java/com/ucm/tfg/commands/CommandAddFriends.java
@@ -3,9 +3,14 @@ package com.ucm.tfg.commands;
 import android.app.Activity;
 import android.util.Log;
 
+import com.ucm.tfg.entities.Friendship;
 import com.ucm.tfg.service.FriendshipService;
 import com.ucm.tfg.service.Service;
 import com.ucm.tfg.service.UserService;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class CommandAddFriends implements Command {
 
@@ -15,6 +20,15 @@ public class CommandAddFriends implements Command {
     @Override
     public Object execute(Object... objects) {
         Log.wtf("command addfriends", objects.toString());
+        Friendship tfriendship = new Friendship();
+        DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        //Log.wtf("command addfriends",sdf.format(date) );
+        tfriendship.setDate(sdf.format(date));
+        tfriendship.setRequesterUuid("1");
+        //tfriendship.setFriendUuid((String) objects[1]);
+        tfriendship.setFriendUuid("2");
+        FriendshipService.addFriend((Activity) objects[0], tfriendship, (Service.ClientResponse) objects[2], (Class) objects[3]);
         //FriendshipService.addFriend((Activity) objects[0],(String) objects[1], "1", (Service.ClientResponse) objects[2], (Class) objects[3]);
         return null;
     }
