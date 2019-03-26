34fabb4338b054f8da2bf2229c5e980c5f0b6253
ZReKoJ <zhong.9745@gmail.com>
2019-03-26 12:46:44 +0100

    plan activity upper done

--- src/main/java/com/ucm/tfg/activities/PlanActivity.java
+++ src/main/java/com/ucm/tfg/activities/PlanActivity.java
@@ -1,10 +1,17 @@
 package com.ucm.tfg.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
 import com.ucm.tfg.R;
 import com.ucm.tfg.entities.Plan;
 
@@ -15,12 +22,41 @@ public class PlanActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plan);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
         Plan plan = (Plan) getIntent().getExtras().getSerializable("plan");
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setTitle(plan.getFilm().getName());
+        }
+
+        ImageView filmPoster = findViewById(R.id.film_poster);
+        Picasso.get()
+                .load(plan
+                        .getFilm()
+                        .getImageURL()
+                )
+                .into(filmPoster);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.film_info);
+        floatingActionButton.setOnClickListener((View v) -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(plan.getFilm().getInfoURL()));
+            startActivity(intent);
+        });
+
         Toast.makeText(PlanActivity.this, plan.getFilm().getName(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
