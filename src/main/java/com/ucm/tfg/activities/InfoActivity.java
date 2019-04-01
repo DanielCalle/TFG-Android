d557d40c8aa7f7dbce060527b4e52b0b9a19aa0d
ZReKoJ <zhong.9745@gmail.com>
2019-04-01 22:58:27 +0200

    added valoration in info activity

--- src/main/java/com/ucm/tfg/activities/InfoActivity.java
+++ src/main/java/com/ucm/tfg/activities/InfoActivity.java
@@ -2,13 +2,18 @@ package com.ucm.tfg.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.ucm.tfg.R;
@@ -46,6 +51,29 @@ public class InfoActivity extends AppCompatActivity {
         Picasso.get()
                 .load(film.getImageURL())
                 .into(filmPoster);
+
+        ProgressBar progressBar = findViewById(R.id.progress_bar);
+        TextView progressText = findViewById(R.id.progress_text);
+        SeekBar progressController = findViewById(R.id.progress_controller);
+
+        progressController.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressBar.setProgress(progress);
+                progressText.setText("" + progress / 10.0 + "/10");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
 
     }
 
---
+++ src/main/java/com/ucm/tfg/views/AnchorVisibilityBehavior.java
@@ -0,0 +1,62 @@
+package com.ucm.tfg.views;
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.widget.ViewGroupUtils;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class AnchorVisibilityBehavior<T extends View> extends CoordinatorLayout.Behavior<T> {
+
+    private Rect mTmpRect;
+
+    public AnchorVisibilityBehavior() { }
+
+    public AnchorVisibilityBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, T child, View dependency) {
+        // check that our dependency is the AppBarLayout
+        return dependency instanceof AppBarLayout;
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, T child, View dependency) {
+        if (dependency instanceof AppBarLayout) {
+            return updateProgressBarVisibility(parent, (AppBarLayout) dependency, child);
+        }
+        return false;
+    }
+
+    private boolean updateProgressBarVisibility(CoordinatorLayout parent, AppBarLayout appBarLayout, T child) {
+        final CoordinatorLayout.LayoutParams lp =
+                (CoordinatorLayout.LayoutParams) child.getLayoutParams();
+        if (lp.getAnchorId() != appBarLayout.getId()) {
+            // The anchor ID doesn't match the dependency, so we won't automatically
+            // show/hide the FAB
+            return false;
+        }
+
+        if (mTmpRect == null) {
+            mTmpRect = new Rect();
+        }
+
+        // First, let's get the visible rect of the dependency
+        final Rect rect = mTmpRect;
+        ViewGroupUtils.getDescendantRect(parent, appBarLayout, rect);
+
+        if (rect.bottom <= appBarLayout.getMinimumHeightForVisibleOverlappingContent()) {
+            // If the anchor's bottom is below the seam, hide it
+            child.setVisibility(View.INVISIBLE);
+        } else {
+            // Else, show it
+            child.setVisibility(View.VISIBLE);
+        }
+        return true;
+    }
+
+}
