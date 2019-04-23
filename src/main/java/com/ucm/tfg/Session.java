8b761e1396a9c06954927c24dd051d257cffa23e
ZReKoJ <zhong.9745@gmail.com>
2019-04-23 17:01:54 +0200

    added user uuid in sharedpreferences

--- src/main/java/com/ucm/tfg/Session.java
+++ src/main/java/com/ucm/tfg/Session.java
@@ -4,4 +4,5 @@ public class Session {
 
     public final static String SESSION_FILE = "SESSION_FILE";
     public final static String IS_LOGGED = "IS_LOGGED";
+    public final static String USER = "SESSION_USER";
 }
